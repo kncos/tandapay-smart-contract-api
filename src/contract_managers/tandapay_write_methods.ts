6bba27608e907d6db1ceac069cf4c8ea7849464d
KevinCPP <kevinncosby2@gmail.com>
2025-02-13 16:47:27 -0500

    .

--- src/contract_managers/tandapay_write_methods.ts
+++ src/contract_managers/tandapay_write_methods.ts
@@ -1,5 +1,5 @@
 import { Address, getContract, Hash, TransactionReceipt } from "viem";
-import { TandaPayContract, WriteableClient } from "../types";
+import { TandaPayContract, WriteableClient } from "types";
 import { TandaPayInfo } from "../_contracts/TandaPay";
 import { waitForTransactionReceipt } from "viem/actions";
 
@@ -7,50 +7,48 @@ import { waitForTransactionReceipt } from "viem/actions";
 export type TandaPayWriteMethodReturnType = Promise<Hash | TransactionReceipt>;
 
 /** Abstract class template for all TandaPay smart contract write interactions. */
-export abstract class TandaPayWriteMethods<
-  TClient extends WriteableClient = WriteableClient,
-> {
-  /** client used to construct the contract instance. */
-  protected client: TClient;
-  /** instance of the tandapay contract we are interacting with */
-  protected contractInstance: TandaPayContract<TClient>;
-  /** If true, all write methods will wait for the transaction to be included in a block (one confirmation) */
-  public waitForTransactionReceipts: boolean = true;
+export abstract class TandaPayWriteMethods<TClient extends WriteableClient = WriteableClient> {
+    /** client used to construct the contract instance. */
+    protected client: TClient;
+    /** instance of the tandapay contract we are interacting with */
+    protected contractInstance: TandaPayContract<TClient>;
+    /** If true, all write methods will wait for the transaction to be included in a block (one confirmation) */
+    public waitForTransactionReceipts: boolean = true;
 
-  /** shorthand for referencing the contractInstance's simulate method */
-  protected get simulate() {
-    return this.contractInstance.simulate;
-  }
-
-  /** shorthand for referencing the contractInstance's write method */
-  protected get write() {
-    return this.contractInstance.write;
-  }
-
-  /**
-   * Deals with hashes at the end of every write method call. if `waitForTransactionReceipts` is set
-   * to true, then it waits for the transaction to be included in a block (one confirmation) then
-   * returns the transaction receipt
-   */
-  protected async handleHash(hash: Hash): TandaPayWriteMethodReturnType {
-    if (this.waitForTransactionReceipts) {
-      return await waitForTransactionReceipt(this.client, { hash });
+    /** shorthand for referencing the contractInstance's simulate method */
+    protected get simulate() {
+        return this.contractInstance.simulate;
     }
 
-    return hash;
-  }
+    /** shorthand for referencing the contractInstance's write method */
+    protected get write() {
+        return this.contractInstance.write;
+    }
 
-  /**
-   * @param client Any client that extends `WriteableClient`, e.g. has WalletActions, an Account, and
-   * a Chain.
-   * @param address The address of the smart contract
-   */
-  constructor(client: TClient, address: Address) {
-    this.client = client;
-    this.contractInstance = getContract({
-      abi: TandaPayInfo.abi,
-      address: address,
-      client: client,
-    });
-  }
+    /** 
+     * Deals with hashes at the end of every write method call. if `waitForTransactionReceipts` is set 
+     * to true, then it waits for the transaction to be included in a block (one confirmation) then
+     * returns the transaction receipt
+    */
+    protected async handleHash(hash: Hash): TandaPayWriteMethodReturnType {
+        if (this.waitForTransactionReceipts) {
+            return await waitForTransactionReceipt(this.client, { hash });
+        } 
+
+        return hash;
+    }
+
+    /**
+     * @param client Any client that extends `WriteableClient`, e.g. has WalletActions, an Account, and
+     * a Chain.
+     * @param address The address of the smart contract
+     */
+    constructor(client: TClient, address: Address) {
+        this.client = client;
+        this.contractInstance = getContract({
+            abi: TandaPayInfo.abi,
+            address: address,
+            client: client,
+        });
+    }
 }
