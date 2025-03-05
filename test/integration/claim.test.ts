56430b0c86bf9db8b715703561cdabed58164172
KevinCPP <kevinncosby2@gmail.com>
2025-03-05 11:38:12 -0500

    .

--- test/integration/claim.test.ts
+++ test/integration/claim.test.ts
@@ -1,9 +1,7 @@
 import { ChildProcess } from "child_process";
 import {
   deployFaucetToken,
-  deployTandaPay,
-  getFtkTransactions,
-  spawnAnvil,
+  deployTandaPay, spawnAnvil
 } from "../helpers/tandapay_test_helpers";
 import { TandaPayTestSuite } from "../helpers/tandapay_test_suite";
 import {
@@ -11,9 +9,10 @@ import {
   TandaPayLog,
   toTandaPayLogs,
 } from "tandapay_manager/read/types";
-import { DumpStateReturnType, formatEther } from "viem";
 import { DEFAULT_CLAIMANT_INDEX, DEFAULT_DEFECTOR } from "../test_config";
-import { memberInfoJsonReplacer } from "types";
+import { memberInfoJsonReplacer, MemberStatus } from "types";
+import * as fs from 'fs/promises';
+import { isAddressEqual } from "viem";
 
 let anvil: ChildProcess;
 let suite: TandaPayTestSuite;
@@ -104,32 +103,49 @@ describe("testing claims, defectors, etc.", () => {
 
   it("Defection works", async () => {
     await suite.toPeriodAfterClaim({alreadyInDefault: false});
-    const block = await suite.testClient.getBlock();
 
-    const defectors = [DEFAULT_DEFECTOR, 5];
+    let e = await suite.advanceTimeAndDefect({include: [DEFAULT_DEFECTOR]});
+    console.log(e);
+    e = await suite.advanceTimeAndWithdrawClaimFund({include: [DEFAULT_CLAIMANT_INDEX]});
+    console.log(e);
+    e = await suite.advanceTimeAndPayPremiums({exclude: [DEFAULT_DEFECTOR]});
+    console.log(e);
+    e = await suite.advanceTimeAndAdvancePeriod();
+    console.log(e);
 
-    const errors: string[] = [];
-    errors.push(...(await suite.advanceTimeAndDefect({include: [DEFAULT_DEFECTOR]})));
-    errors.push(...(await suite.advanceTimeAndPayPremiums({exclude: [DEFAULT_DEFECTOR]})));
-    errors.push(...(await suite.advanceTimeAndAdvancePeriod()));
-    if (errors.length > 0)
-      console.log(errors.join('\n'));
-
-    let tandapayLogs = toTandaPayLogs((await suite.secretary.events.getLogs({
+    let tandaPayLogs = toTandaPayLogs((await suite.secretary.events.getLogs({
       fromBlock: 0n,
       toBlock: 'latest',
-    }))).filter(log => !logsToOmit.includes(log.alias));
+    }))).filter(log => log.alias === "memberDefected");
 
-    // print all log aliases except the ones we omitted
-    //console.log(tandapayLogs.find(l => l.alias === "memberDefected"));
-
-    for (let i = 1n; i <= 15n; i++) {
-      let mInfo = await suite.secretary.read.getMemberInfoFromId(i);
-      console.log(JSON.stringify(mInfo,memberInfoJsonReplacer,2));
+    const curPeriod = await suite.secretary.read.getCurrentPeriodId();
+    console.log(curPeriod);
+    for (let memberId = 1n; memberId <= 15n; memberId++) {
+      let memberInfo = await suite.secretary.read.getMemberInfoFromId(memberId, curPeriod-1n);
+      console.log(memberInfo);
     }
 
-    let state = await suite.secretary.read.getCommunityState();
-    //console.log(state)
+    console.log(tandaPayLogs);
+    for (const log of tandaPayLogs) {
+      if (log.alias !== "memberDefected")
+        fail(`check tandaPayLogs filter. found log that wasn't \'memberDefected\': ${log.alias}`);
+
+      const l = log as TandaPayLog<'memberDefected'>;
+      const defectorAddress = l.args.member ?? fail('defectorAddress was undefined');
+      const defectorInfo = await suite.secretary.read.getMemberInfoFromId(BigInt(DEFAULT_DEFECTOR)+1n);
+      expect(isAddressEqual(defectorAddress, defectorInfo.walletAddress)).toBe(true);
+      // make sure they are marked as defected
+      expect(defectorInfo.memberStatus).toBe(MemberStatus.Defected);
+      // they shouldn't have any funds in their savings or community escrow (it should all be in pending)
+      expect(defectorInfo.communityEscrowAmount).toBe(0n);
+      expect(defectorInfo.savingsEscrowAmount).toBe(0n);
+      // they shouldn't have coverage this period or have their premium paid
+      expect(defectorInfo.isEligibleForCoverageThisPeriod).toBe(false);
+      expect(defectorInfo.isPremiumPaidThisPeriod).toBe(false);
+      // their subgroup id should be "0" -- in TandaPay's smart contract, subgroup IDs seem to start
+      // from 1, with 0 being used as a placeholder for "invalid" -- which makes sense if they defected.
+      expect(defectorInfo.subgroupId).toBe(0n); 
+    }
   });
 
 });
