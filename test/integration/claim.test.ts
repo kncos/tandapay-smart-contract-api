1f4e66fc2751a57f7361b1e3f1b11ff1a56550cf
KevinCPP <kevinncosby2@gmail.com>
2025-03-06 14:53:48 -0500

    trying to figure out why the community collapsed

--- test/integration/claim.test.ts
+++ test/integration/claim.test.ts
@@ -10,7 +10,7 @@ import {
   toTandaPayLogs,
 } from "tandapay_manager/read/types";
 import { DEFAULT_CLAIMANT_INDEX, DEFAULT_DEFECTOR } from "../test_config";
-import { memberInfoJsonReplacer, MemberStatus } from "types";
+import { memberInfoJsonReplacer, MemberStatus, periodInfoJsonReplacer, TandaPayState } from "types";
 import * as fs from 'fs/promises';
 import { isAddressEqual } from "viem";
 
@@ -101,51 +101,73 @@ describe("testing claims, defectors, etc.", () => {
     expect(afterBalance-claimInfo.amount).toBe(beforeBalance);
   }, 30000);
 
-  it("Defection works", async () => {
-    await suite.toPeriodAfterClaim({alreadyInDefault: false});
+  const defectorSetup = async (claimants: number[], defectors: number[]) => {
+    await suite.toPeriodAfterClaim({alreadyInDefault: false, claimants: claimants, wontPayPremium: defectors});
 
-    let e = await suite.advanceTimeAndDefect({include: [DEFAULT_DEFECTOR]});
-    console.log(e);
-    e = await suite.advanceTimeAndWithdrawClaimFund({include: [DEFAULT_CLAIMANT_INDEX]});
-    console.log(e);
-    e = await suite.advanceTimeAndPayPremiums({exclude: [DEFAULT_DEFECTOR]});
-    console.log(e);
-    e = await suite.advanceTimeAndAdvancePeriod();
-    console.log(e);
+    const ops = [
+      async () => await suite.advanceTimeAndDefect({include: defectors}),
+      async () => await suite.advanceTimeAndWithdrawClaimFund({include: claimants}),
+      async () => await suite.advanceTimeAndPayPremiums({exclude: defectors}),
+      async () => await suite.advanceTimeAndAdvancePeriod(),
+    ]
 
+    const allErrors: string[] = [];
+    for (const op of ops) {
+      const errors = await op();
+      if (errors && errors.length > 0)
+        allErrors.push(...errors);
+    }
+
+    return allErrors;
+  }
+
+  it("one defector, doesn't pay last premium, community remains in default state. *Defector should become USER_QUIT*", async () => {
+    // perform setup
+    const claimants = [DEFAULT_CLAIMANT_INDEX];
+    const defectors = [DEFAULT_DEFECTOR];
+    const errors = await defectorSetup(claimants, defectors);
+    if (errors && errors.length > 0) 
+      console.warn(errors);
+
+    // this will get logs for when someone defects, regardless of whether or not
+    // their status is updated to "defected" or something else like 'user-quit"/"unpaid-invalid"
     let tandaPayLogs = toTandaPayLogs((await suite.secretary.events.getLogs({
       fromBlock: 0n,
       toBlock: 'latest',
     }))).filter(log => log.alias === "memberDefected");
 
-    const curPeriod = await suite.secretary.read.getCurrentPeriodId();
-    console.log(curPeriod);
-    for (let memberId = 1n; memberId <= 15n; memberId++) {
-      let memberInfo = await suite.secretary.read.getMemberInfoFromId(memberId, curPeriod-1n);
-      console.log(memberInfo);
-    }
+    // we expect there to be an event emitted for each defector
+    expect(tandaPayLogs.length).toBe(defectors.length);
 
-    console.log(tandaPayLogs);
+    // go through each log
     for (const log of tandaPayLogs) {
+      // the logs should be properly filtered to just memberDefected logs
       if (log.alias !== "memberDefected")
         fail(`check tandaPayLogs filter. found log that wasn't \'memberDefected\': ${log.alias}`);
 
+      // narrow the type
       const l = log as TandaPayLog<'memberDefected'>;
+      // check that the address in the event matches our defector's address
       const defectorAddress = l.args.member ?? fail('defectorAddress was undefined');
       const defectorInfo = await suite.secretary.read.getMemberInfoFromId(BigInt(DEFAULT_DEFECTOR)+1n);
       expect(isAddressEqual(defectorAddress, defectorInfo.walletAddress)).toBe(true);
-      // make sure they are marked as defected
-      expect(defectorInfo.memberStatus).toBe(MemberStatus.Defected);
+
+      //! here, we expect them to be UserQuit because we're still in the default state
+      expect(defectorInfo.memberStatus).toBe(MemberStatus.UserQuit);
+
       // they shouldn't have any funds in their savings or community escrow (it should all be in pending)
       expect(defectorInfo.communityEscrowAmount).toBe(0n);
       expect(defectorInfo.savingsEscrowAmount).toBe(0n);
       // they shouldn't have coverage this period or have their premium paid
       expect(defectorInfo.isEligibleForCoverageThisPeriod).toBe(false);
       expect(defectorInfo.isPremiumPaidThisPeriod).toBe(false);
-      // their subgroup id should be "0" -- in TandaPay's smart contract, subgroup IDs seem to start
-      // from 1, with 0 being used as a placeholder for "invalid" -- which makes sense if they defected.
-      expect(defectorInfo.subgroupId).toBe(0n); 
     }
+
+    const communityState = await suite.secretary.read.getCommunityState();
+    expect(communityState).toBe(TandaPayState.Default);
+    const currentPeriod = await suite.secretary.read.getCurrentPeriodId();
+    const defectorIds = await suite.secretary.read.getClaimIdsInPeriod(currentPeriod-1n);
+    expect(defectorIds.length).toBe(defectors.length);
   });
 
 });
